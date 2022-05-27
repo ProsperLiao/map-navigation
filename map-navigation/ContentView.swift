dcdb9d69065cf24613c98786f30d57bd3c6d43cb
Hongxing Liao <hx.liao@gmail.com>
2022-05-27 16:38:28 +0800

    Init: initialize the xcode project

---
+++ map-navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  map-navigation
+//
+//  Created by Hongxing Liao on 2022/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ map-navigation/map_navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_navigationApp.swift
+//  map-navigation
+//
+//  Created by Hongxing Liao on 2022/5/27.
+//
+
+import SwiftUI
+
+@main
+struct map_navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
